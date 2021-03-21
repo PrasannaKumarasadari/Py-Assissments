9bb63a8f0b547851c4ed09c1336ecc0c621ced7d
Prasanna Kumar Sadari <79400105+PrasannaKumarasadari@users.noreply.github.com>
2021-03-21 18:43:48 +0530

    Add files via upload

---
+++ Programming Assignment-1.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+##Question 1##
+print("Hello Python")
+
+
+# In[2]:
+
+
+##Question 2##
+k = 2
+j = 8
+
+
+# In[4]:
+
+
+l = k + j
+
+
+# In[5]:
+
+
+l
+
+
+# In[7]:
+
+
+j/k
+
+
+# In[8]:
+
+
+##Question 3##
+Side_of_a_triangle_a = 7
+Height_of_triangle_b = 8
+
+
+# In[9]:
+
+
+Area = 0.5 * Side_of_a_triangle_a * Height_of_triangle_b
+
+
+# In[10]:
+
+
+Area
+
+
+# In[ ]:
+
+
+##Question 4##
+
+
+# In[11]:
+
+
+c = 9
+
+
+# In[12]:
+
+
+o = 10
+
+
+# In[13]:
+
+
+o = c
+
+
+# In[14]:
+
+
+o
+
+
+# In[ ]:
+
+
+##Question 5##
+
+
+# In[20]:
+
+
+def gen(integer):
+    for i in range(0,integer):
+        
+        print(i)
+
+
+# In[21]:
+
+
+gen(25)
+
+
+# In[ ]:
+
+
+
+
